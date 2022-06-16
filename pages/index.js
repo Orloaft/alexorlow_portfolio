e07b89cb33eb64eca262544ba6c516b823f1152b
Orloaft <alexorlow17@gmail.com>
2022-06-15 20:01:49 -0400

    add dashboard component and sass

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,10 @@
 import styles from "../styles/Home.module.css";
+import { Dashboard } from "../components/Dashboard/Dashboard";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      {" "}
-      Vegan cozy butternut broccoli almonds blood orange smash açai falafel
-      bites frosted gingerbread bites fruit smash scotch bonnet pepper creamy
-      cauliflower alfredo farro platter lychee lemon tahini dressing summer
-      couscous edamame hummus crumbled lentils cumin jalapeño Thai super chili
-      oranges samosa cranberry spritzer black beans dark and stormy ginger tofu
-      apples. Crispy simmer kung pao pepper orange cherry bomb pepper
-      pomegranate springtime strawberry green tea lime hummus bento box sweet
-      potato matcha casserole kale caesar salad lemongrass hummus falafel bowl
-      main course green onions. Cool cucumbers mocha chocolate cherry bomb salad
-      dragon fruit tofu green grapes sesame soba noodles hazelnut shiitake
-      mangos blueberry chia seed jam roasted peanuts.{" "}
+      <Dashboard />
     </div>
   );
 }
